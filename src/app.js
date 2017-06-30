2399c74ea8e1d90309715af3588df4a25c1bcfd4
Hansmu <hans.miikael@gmail.com>
2017-06-30 13:24:11 +0300

    Angular watchers

--- src/app.js
+++ src/app.js
@@ -8,4 +8,18 @@ myFirstApplication.controller('mainController', ['$scope', '$filter', function (
     $scope.lowercasehandle = function () {
         return $filter('lowercase')($scope.myInputField);
     };
+
+    $scope.$watch('handle', function(newValue, oldValue) {
+        console.info('Changed!', newValue, oldValue);
+    });
+
+    //  This starts up a new thread without the AngularJS context. Have to add additional things in order to make Angular realize a change has happened.
+    setTimeout(function() {
+        $scope.$apply(function() {  //  This tells Angular JS to run its normal process when the variables change. This makes Angular aware.
+            //  Angular wraps most of its services inside of $apply, so they update the view etc. $timeout vs setTimeout
+            //  When using external libraries that aren't wrapped in $apply you need to do it manually like here.
+            $scope.myInputField = 'newtiwtterlhanda';
+            console.log('Scope changed!');
+        });
+    }, 3000);
 }]);
