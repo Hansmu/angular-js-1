d926c669ceeb92d374db403dbcde5044bf9b8831
Hansmu <hans.miikael@gmail.com>
2017-06-30 13:47:58 +0300

    Angular directives.

--- src/app.js
+++ src/app.js
@@ -9,17 +9,15 @@ myFirstApplication.controller('mainController', ['$scope', '$filter', function (
         return $filter('lowercase')($scope.myInputField);
     };
 
-    $scope.$watch('handle', function(newValue, oldValue) {
-        console.info('Changed!', newValue, oldValue);
-    });
+    $scope.characters = 5;
 
-    //  This starts up a new thread without the AngularJS context. Have to add additional things in order to make Angular realize a change has happened.
-    setTimeout(function() {
-        $scope.$apply(function() {  //  This tells Angular JS to run its normal process when the variables change. This makes Angular aware.
-            //  Angular wraps most of its services inside of $apply, so they update the view etc. $timeout vs setTimeout
-            //  When using external libraries that aren't wrapped in $apply you need to do it manually like here.
-            $scope.myInputField = 'newtiwtterlhanda';
-            console.log('Scope changed!');
-        });
-    }, 3000);
+    $scope.rules = [
+        { rulename: "Must be 5 characters." },
+        { rulename: "Must not be used elsewhere." },
+        { rulename: "Must be cool." }
+    ];
+
+    $scope.alertClick = function() {
+        alert("Clicked!");
+    };
 }]);
