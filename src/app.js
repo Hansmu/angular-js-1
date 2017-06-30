db3f7743053bf731898daef5feb64c668b14115e
Hansmu <hans.miikael@gmail.com>
2017-06-30 12:43:02 +0300

    Passing variable from model to view.

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 var myFirstApplication = angular.module('myApp', ['ngMessages', 'ngResource']);   //  Second parameter is a list of modules that my app is dependent on.
 //  Once you reference/inject a dependency, then you can inject its objects into your functions or in the view.
 
-myFirstApplication.controller('mainController', ['$scope', '$log', '$resource', function ($scope, $log, $resource) { //  Using the previous version will cause errors when minifying.
+myFirstApplication.controller('mainController', ['$scope', '$timeout', function ($scope, $timeout) { //  Using the previous version will cause errors when minifying.
     //  With this version - a minifier will never change what's inside a string. With this the positions have to match, as there's no name to inject on.
 
-    $log.log($resource);
+   $scope.myMessage = 'Bananas are illegal';
+
+   $timeout(function() {
+       $scope.myMessage = 'Timeout message';    //  View gets automatically updated when the variable gets changed.
+   }, 3000);
 }]);
