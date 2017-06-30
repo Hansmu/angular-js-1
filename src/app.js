a61a574e67016cc564ed807b885627df5c0e2d89
Hansmu <hans.miikael@gmail.com>
2017-06-30 12:31:13 +0300

    Dependency injection that works with minifcation but depends on order.

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 var myFirstApplication = angular.module('myApp', ['ngMessages', 'ngResource']);   //  Second parameter is a list of modules that my app is dependent on.
 //  Once you reference/inject a dependency, then you can inject its objects into your functions or in the view.
 
-myFirstApplication.controller('mainController', function ($scope, $log, $resource) { //  Scope is just whatever variable, just add the dollar sign to easily recognize it as an Angular JS service.
-    //  Actually isn't whatever. Angular uses the name to inject dependencies, not the number of variable. So for example $log injects the service named $log that Angular has.
+myFirstApplication.controller('mainController', ['$scope', '$log', '$resource', function ($scope, $log, $resource) { //  Using the previous version will cause errors when minifying.
+    //  With this version - a minifier will never change what's inside a string. With this the positions have to match, as there's no name to inject on.
 
     $log.log($resource);
-});
+}]);
