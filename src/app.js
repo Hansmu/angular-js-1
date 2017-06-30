c2108bc76230669647725b46dc3e73f91bd9a997
Hansmu <hans.miikael@gmail.com>
2017-06-30 13:07:18 +0300

    Two way data binding example

--- src/app.js
+++ src/app.js
@@ -1,12 +1,11 @@
 var myFirstApplication = angular.module('myApp', ['ngMessages', 'ngResource']);   //  Second parameter is a list of modules that my app is dependent on.
 //  Once you reference/inject a dependency, then you can inject its objects into your functions or in the view.
 
-myFirstApplication.controller('mainController', ['$scope', '$timeout', function ($scope, $timeout) { //  Using the previous version will cause errors when minifying.
-    //  With this version - a minifier will never change what's inside a string. With this the positions have to match, as there's no name to inject on.
+myFirstApplication.controller('mainController', ['$scope', '$filter', function ($scope, $filter) {
 
-   $scope.myMessage = 'Bananas are illegal';
+    $scope.myInputField = '';
 
-   $timeout(function() {
-       $scope.myMessage = 'Timeout message';    //  View gets automatically updated when the variable gets changed.
-   }, 3000);
+    $scope.lowercasehandle = function () {
+        return $filter('lowercase')($scope.myInputField);
+    };
 }]);
