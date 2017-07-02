5215b71203c646cc7594190cd092af0e4327ac26
hansmu <hans.miikael@gmail.com>
2017-07-02 16:06:50 +0300

    HTTP GET and POST examples

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 var myFirstApplication = angular.module('myApp', ['ngMessages', 'ngResource']);   //  Second parameter is a list of modules that my app is dependent on.
 //  Once you reference/inject a dependency, then you can inject its objects into your functions or in the view.
 
-myFirstApplication.controller('mainController', ['$scope', '$filter', function ($scope, $filter) {
+myFirstApplication.controller('mainController', ['$scope', '$filter', '$http', function ($scope, $filter, $http) {
 
     $scope.myInputField = '';
 
@@ -19,5 +19,19 @@ myFirstApplication.controller('mainController', ['$scope', '$filter', function (
 
     $scope.alertClick = function() {
         alert("Clicked!");
+    };
+
+    $http.get('https://jsonplaceholder.typicode.com/users')
+        .then(function(response) {
+            $scope.users = response.data;
+        });
+
+    $scope.addUser = function () {
+        $http.post('https://jsonplaceholder.typicode.com/users',
+            { name: $scope.newUserName })
+            .then(function(response) {
+                $scope.newUserName = '';
+                console.log(response);
+            });
     };
 }]);
