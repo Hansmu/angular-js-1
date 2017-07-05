a2049326a602bed4a5f5e9d1d18b825a3beaefec
hansmu <hans.miikael@gmail.com>
2017-07-05 20:57:40 +0300

    Routing

--- src/app.js
+++ src/app.js
@@ -1,37 +1,32 @@
-var myFirstApplication = angular.module('myApp', ['ngMessages', 'ngResource']);   //  Second parameter is a list of modules that my app is dependent on.
+var myFirstApplication = angular.module('myApp', ['ngRoute']);   //  Second parameter is a list of modules that my app is dependent on.
 //  Once you reference/inject a dependency, then you can inject its objects into your functions or in the view.
 
-myFirstApplication.controller('mainController', ['$scope', '$filter', '$http', function ($scope, $filter, $http) {
+myFirstApplication.config(function($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'pages/main.html',
+            controller: 'mainController'
+        })
+        .when('/second', {
+            templateUrl: 'pages/second.html',
+            controller: 'secondController'
+        })
+        .when('/second/:someValue', {
+            templateUrl: 'pages/second.html',
+            controller: 'secondController'
+        });
+});
+
+myFirstApplication.controller('mainController', ['$scope', '$location', '$http', function ($scope, $location, $http) {
 
     $scope.myInputField = '';
 
-    $scope.lowercasehandle = function () {
-        return $filter('lowercase')($scope.myInputField);
-    };
+}]);
 
-    $scope.characters = 5;
+myFirstApplication.controller('secondController', ['$scope', '$location', '$http', '$routeParams',  function ($scope, $location, $http, $routeParams) {
 
-    $scope.rules = [
-        { rulename: "Must be 5 characters." },
-        { rulename: "Must not be used elsewhere." },
-        { rulename: "Must be cool." }
-    ];
+    $scope.myInputField = '';
+    $scope.value = $routeParams.someValue || 1;
 
-    $scope.alertClick = function() {
-        alert("Clicked!");
-    };
 
-    $http.get('https://jsonplaceholder.typicode.com/users')
-        .then(function(response) {
-            $scope.users = response.data;
-        });
-
-    $scope.addUser = function () {
-        $http.post('https://jsonplaceholder.typicode.com/users',
-            { name: $scope.newUserName })
-            .then(function(response) {
-                $scope.newUserName = '';
-                console.log(response);
-            });
-    };
 }]);
